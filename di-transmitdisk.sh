aa333e3f062d75e36af3465a2d0c24cd55d592ec
TJ Luoma <luomat@gmail.com>
2015-10-16 21:20:17 -0400

    commentary added

--- di-transmitdisk.sh
+++ di-transmitdisk.sh
@@ -19,12 +19,19 @@ URL='https://download.panic.com/transmit/Transmit%20Disk%204.4.9.pkg'
 
 NAME="$0:t:r"
 
+	# create the filename by referencing the last part of the URL
+	# and putting it into the DIR referenced above
 FILE="$DIR/$URL:t"
 
+	# Download the pkg from the URL
+	# (If it already exists and is fully downloaded, curl will give a harmless error)
 curl --continue-at - --fail --location --progress-bar --output "$FILE" "$URL"
 
+	# This is where it actually gets installed.
+	# Note the lack of 'sudo' - this is intentional
 installer -pkg "$FILE" -target CurrentUserHomeDirectory -lang en 2>&1
 
+	# make sure the `installer` worked
 EXIT="$?"
 
 if [ "$EXIT" = "0" ]
